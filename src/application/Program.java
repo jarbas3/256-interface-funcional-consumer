0c324628ca952d86966e7732189eba5caae89e26
Jarbas Santos <jarbas3@outlook.com>
2023-07-20 18:30:05 -0300

    Reference Method com método estático.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 	
@@ -19,7 +19,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(new PriceUpdate());
+		list.forEach(Product::staticPriceUpdate);
 		list.forEach(System.out::println);
 	}
 }
